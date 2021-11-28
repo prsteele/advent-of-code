8d3ba601ed78a4abe72f09e08c9482547dea9e94
Patrick Steele <steele.pat@gmail.com>
2021-11-28 17:49:27 -0500

    Add tests

--- src/Aoc/Y2020/P1.hs
+++ src/Aoc/Y2020/P1.hs
@@ -4,8 +4,9 @@ module Aoc.Y2020.P1 where
 
 import Aoc (Solution)
 import Aoc.Parsers (Parser)
+import Data.Functor ((<&>))
 import qualified Aoc.Parsers as P
-import Aoc.Sort (mutInit, sort)
+import Aoc.Sort (sort')
 import qualified Data.Vector.Unboxed as V
 import Text.Megaparsec (eof)
 import Text.Megaparsec.Char (space)
@@ -15,26 +16,29 @@ parser = V.fromList <$> P.delimited P.int space <* eof
 
 solution :: Solution
 solution input = do
-  mv <- P.parse parser input >>= mutInit
-  sort mv
-  v <- V.unsafeFreeze mv
+  v <- P.parse parser input
   solvePart1 2020 v
   solvePart2 2020 v
 
 solvePart1 :: Int -> V.Vector Int -> IO ()
-solvePart1 target v =
-  case twoSum v target of
+solvePart1 target v = do
+  result <- twoSum v target
+  case result of
     Nothing -> putStrLn "Part 1: Failed to find sum"
     Just x -> print x
 
 solvePart2 :: Int -> V.Vector Int -> IO ()
-solvePart2 target v =
-  case threeSum v target of
+solvePart2 target v = do
+  result <- threeSum v target
+  case result of
     Nothing -> putStrLn "Part 2: Failed to find sum"
     Just x -> print x
 
-twoSum :: V.Vector Int -> Int -> Maybe Int
-twoSum v target =
+twoSum :: V.Vector Int -> Int -> IO (Maybe (Int, Int))
+twoSum v target = sort' v <&> flip twoSum' target
+
+twoSum' :: V.Vector Int -> Int -> Maybe (Int, Int)
+twoSum' v target =
   let search i j
         | i >= j = Nothing
         | otherwise =
@@ -42,17 +46,20 @@ twoSum v target =
               jx = v V.! j
            in case compare (ix + jx) target of
                 LT -> search (i + 1) j
-                EQ -> Just (ix * jx)
+                EQ -> Just (ix , jx)
                 GT -> search i (j - 1)
    in search 0 (V.length v - 1)
 
-threeSum :: V.Vector Int -> Int -> Maybe Int
-threeSum v target =
+threeSum ::  V.Vector Int -> Int -> IO (Maybe (Int, Int, Int))
+threeSum v target = sort' v <&> flip threeSum' target
+
+threeSum' :: V.Vector Int -> Int -> Maybe (Int, Int, Int)
+threeSum' v target =
   let len = V.length v
       search i
         | i >= len - 2 = Nothing
         | otherwise =
-          case twoSum (V.slice (i + 1) (len - i - 1) v) (target - v V.! i) of
-            Just x -> Just $ (v V.! i) * x
+          case twoSum' (V.slice (i + 1) (len - i - 1) v) (target - v V.! i) of
+            Just (y, z) -> Just (v V.! i, y, z)
             Nothing -> search (i + 1)
    in search 0
---
+++ test/Aoc/Y2020/P1Spec.hs
@@ -0,0 +1,52 @@
+module Aoc.Y2020.P1Spec where
+
+import qualified Data.Vector.Unboxed as V
+import Test.Hspec
+import Test.QuickCheck
+import Test.QuickCheck.Monadic
+import Aoc.Y2020.P1
+import Data.Maybe (isNothing)
+import Test.Hspec.QuickCheck (prop)
+
+spec :: Spec
+spec = do
+  twoSumSpec
+  threeSumSpec
+
+twoSumSpec :: Spec
+twoSumSpec =
+  describe "twoSum" $ do
+    it "computes sample input correctly" $ do
+      answer <- twoSum (V.fromList [1721, 979, 366, 299, 675, 1456]) 2020
+      answer `shouldBe` Just (299, 1721)
+
+threeSumSpec :: Spec
+threeSumSpec =
+  describe "threeSum" $ do
+    it "computes sample input correctly" $ do
+      answer <- threeSum (V.fromList [1721, 979, 366, 299, 675, 1456]) 2020
+      answer `shouldBe` Just (366, 675, 979)
+    prop "computes correct results on arbitrary inputs" $
+      \nums -> monadicIO $ do
+        let v = V.fromList nums
+        if V.length v < 3
+          then do
+            -- Trivial case where there aren't three elements
+            target <- pick arbitrary
+            result <- run (threeSum v target)
+            assert (isNothing result)
+          else
+            -- Regular case where there are at least three elements
+            let
+              target = V.sum (V.slice 0 3 v)
+            in do
+              result <- run (threeSum v target)
+              case result of
+                Nothing -> assert False
+                Just (x, y, z) -> assert (x + y + z == target) 
+    prop "computes correct results on arbitrary inputs (where there is no match)" $
+      \nums -> monadicIO $ do
+        let v = V.fromList nums
+        let target = sum (filter (> 0) (V.toList v)) + 1
+        result <- run (threeSum v target)
+        assert (isNothing result)
