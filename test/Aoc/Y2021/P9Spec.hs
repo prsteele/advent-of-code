1688a6f8d0d6274a7223fcda179d1d5cad9a540a
Patrick Steele <steele.pat@gmail.com>
2021-12-02 23:45:07 -0500

    Improve test stubs

--- test/Aoc/Y2021/P9Spec.hs
+++ test/Aoc/Y2021/P9Spec.hs
@@ -4,4 +4,18 @@ import Aoc.Y2021.P9
 import Test.Hspec
 
 spec :: Spec
-spec = pure ()
+spec = do
+  part1Spec
+  part2Spec
+
+part1Spec :: Spec
+part1Spec =
+  describe "part 1" $ do
+    it "computes sample input correctly" $ do
+      pending
+
+part2Spec :: Spec
+part2Spec =
+  describe "part 2" $ do
+    it "computes sample input correctly" $ do
+      pending
