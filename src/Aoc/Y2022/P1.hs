105ac4ca4a5eb0decd1a7b51cd362cf455ba0e0b
Patrick Steele <steele.pat@gmail.com>
2022-12-03 15:11:03 -0500

    Solve problem 1

--- src/Aoc/Y2022/P1.hs
+++ src/Aoc/Y2022/P1.hs
@@ -5,15 +5,18 @@ module Aoc.Y2022.P1 where
 import Aoc (Solution)
 import Aoc.Parsers (Parser)
 import qualified Aoc.Parsers as P
-import qualified Data.Text as T
 import qualified Data.Vector as V
+import qualified Data.Vector.Algorithms.Intro as I
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
-type ProblemInput = Int
+type ProblemInput = V.Vector (V.Vector Int)
+
+countGroup :: Parser (V.Vector Int)
+countGroup = V.fromList <$> sepEndBy P.int eol
 
 parser :: Parser ProblemInput
-parser = undefined
+parser = V.fromList <$> sepEndBy countGroup eol <* eof
 
 solution :: Solution
 solution input = do
@@ -21,8 +24,13 @@ solution input = do
   solvePart1 x >>= print
   solvePart2 x >>= print
 
-solvePart1 :: ProblemInput -> IO T.Text
-solvePart1 _ = pure ("Not yet implemented")
+calsPerElf :: ProblemInput -> V.Vector Int
+calsPerElf = fmap V.sum
 
-solvePart2 :: ProblemInput -> IO T.Text
-solvePart2 _ = pure ("Not yet implemented")
+solvePart1 :: ProblemInput -> IO Int
+solvePart1 = pure . maximum . calsPerElf
+
+solvePart2 :: ProblemInput -> IO Int
+solvePart2 cals =
+  let ordered = V.modify I.sort (calsPerElf cals)
+   in pure $ V.sum (V.slice (V.length cals - 3) 3 ordered)
