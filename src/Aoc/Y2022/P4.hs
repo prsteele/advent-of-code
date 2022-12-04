9679638775df63eac096d87b64449777b39bb4e0
Patrick Steele <steele.pat@gmail.com>
2022-12-04 00:09:40 -0500

    Solve problem 4

--- src/Aoc/Y2022/P4.hs
+++ src/Aoc/Y2022/P4.hs
@@ -5,15 +5,35 @@ module Aoc.Y2022.P4 where
 import Aoc (Solution)
 import Aoc.Parsers (Parser)
 import qualified Aoc.Parsers as P
+import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Data.Vector as V
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
-type ProblemInput = Int
+type ProblemInput = [(Range, Range)]
+
+type Range = (Int, Int)
+
+parsePair :: Parser (Range, Range)
+parsePair = do
+  a <- P.int
+  chunk "-"
+  b <- P.int
+  chunk ","
+  c <- P.int
+  chunk "-"
+  d <- P.int
+  pure ((a, b), (c, d))
+
+contains :: (Range, Range) -> Bool
+contains ((a, b), (c, d)) = (a <= c && d <= b) || (c <= a && b <= d)
+
+overlap :: (Range, Range) -> Bool
+overlap ((a, b), (c, d)) = max a c <= min b d
 
 parser :: Parser ProblemInput
-parser = undefined
+parser = P.linesOf parsePair
 
 solution :: Solution
 solution input = do
@@ -21,8 +41,8 @@ solution input = do
   solvePart1 x >>= print
   solvePart2 x >>= print
 
-solvePart1 :: ProblemInput -> IO T.Text
-solvePart1 _ = pure ("Not yet implemented")
+solvePart1 :: ProblemInput -> IO Int
+solvePart1 = pure . length . filter contains
 
-solvePart2 :: ProblemInput -> IO T.Text
-solvePart2 _ = pure ("Not yet implemented")
+solvePart2 :: ProblemInput -> IO Int
+solvePart2 = pure . length . filter overlap
